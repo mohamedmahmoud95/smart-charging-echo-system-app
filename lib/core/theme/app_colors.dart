a0899547b17902629bdd1a768993488643aa80bb
Mohamed Raslan <m.raslan97@gmail.com>
2024-06-08 14:46:22 +0300

    added alert dialog widget

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 class AppColors
 {
   static const Color backgroundWhite = Color(0xFFFAFAFA);
-
+  static const Color yellow = Color(0xFFFFCB47);
+  static const Color green = Color(0xFF074F57);
+  static const Color red = Color(0xFFD32F2F);
+  static const Color textGrey = Color(0xFF909090);
 }
