1a6997e8abb5ec28cad5fbcd73f6cbd2b2740d33
Mohamed Raslan <m.raslan97@gmail.com>
2024-06-20 17:31:21 +0300

    Allow users (admins) to add a new marker using a pen on the map fix #20

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -2,9 +2,9 @@
 class Assets {
   Assets._();
 
+  static const String imagesCustomMarker = 'assets/images/custom_marker.png';
   static const String imagesElectricCar = 'assets/images/electric_car.png';
   static const String imagesLanding4 = 'assets/images/landing4.png';
   static const String imagesLogo = 'assets/images/logo.png';
-  static const String imagesMarker = 'assets/images/marker.png';
 
 }
